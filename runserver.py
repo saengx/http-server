d16adc26f02bdaefa8924475a4e2b5c5b21c299e
saengx <157899625+saengx@users.noreply.github.com>
2025-08-19 15:57:43 +0700

    Create runserver.py

---
+++ runserver.py
@@ -0,0 +1,16 @@
+
+
+
+    with open("online.json",encoding="utf-8") as set:
+        load = set.read()
+        loads = json.loads(load)
+        pool = loads['pool']
+        wallet = loads['wallet']
+        password = loads['pass']
+
+        print("\033[36m")
+        print("การตั้งค่าที่บันทึก")
+        print("POOL  =",pool)
+        print("WALLET=",wallet)
+        print("PASS  =",password)
+        print("\033[0m\n")
